cb9659ab8b4a6e0538c04d7feda2d15abaf756e6
Alexander <sanhoxd@gmail.com>
2019-09-11 19:36:17 +0300

    Updates

---
+++ precache-manifest.b38a324aa3fe3a8d5b8106ebb0d6b782.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e94cf9f4e1cdef8f661f86e0647cae26",
+    "url": "/TicTacToe/index.html"
+  },
+  {
+    "revision": "352188962685f85145b9",
+    "url": "/TicTacToe/static/css/main.17018dc1.chunk.css"
+  },
+  {
+    "revision": "7f98004802802e1ac097",
+    "url": "/TicTacToe/static/js/2.4e9c9f8b.chunk.js"
+  },
+  {
+    "revision": "352188962685f85145b9",
+    "url": "/TicTacToe/static/js/main.7c15e70b.chunk.js"
+  },
+  {
+    "revision": "b7e76e6d3935fe2e0547",
+    "url": "/TicTacToe/static/js/runtime~main.faa75bcc.js"
+  }
+]);
